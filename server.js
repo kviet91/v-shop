2f55aed10c53e494ce5f5ec57ebb896f31c0f51d
kviet91 <kviet91.cdp@gmail.com>
2021-09-10 15:07:23 +0700

    deploy

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
+const path = require('path')
+
 const userRouter = require('./Routers/userRouter')
 const categoryRouter = require('./Routers/categoryRouter')
 const uploadRouter = require('./Routers/upload')
@@ -28,11 +30,19 @@ mongoose.connect(URI, {
     console.log('Connected to MongoDB')
 })
 
+//Router
 app.use('/user', userRouter);
 app.use('/api', categoryRouter);
 app.use('/api', uploadRouter);
 app.use('/api', productRouter);
 app.use('/api', paymentRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
